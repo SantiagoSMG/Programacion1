565f9fd435160b299c0bbb7462751bc43512669f
Santiago <cur3naky17@gmail.com>
2025-10-28 08:08:41 -0700

    agregar

---
+++ IndvDeJuegos.py
@@ -0,0 +1,12 @@
+info_tienda = {
+    'eslogan': 'Game On!',
+    'Año de fundación': 2010 
+}
+print(info_tienda)
+print("Este es el catalogo de juegos")
+inv_juegos = {
+    "Spiderman 2": {"precio":999,"stock":20},
+    "Minecraft": {"precio":799, "stock":22},
+    "pac-man": {"precio": 50, "stock":25}
+}
+print(inv_juegos)
