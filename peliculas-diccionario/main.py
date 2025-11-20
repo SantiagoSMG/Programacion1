0c609e7e0abff40c107ed431da7b9e1c312c1ffa
Santiago <cur3naky17@gmail.com>
2025-11-19 19:36:47 -0700

    ag

--- peliculas-diccionario/main.py
+++ peliculas-diccionario/main.py
@@ -1,12 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
-from modelos.genero import generoPel
+from modelos.genero import mostrar_genero
 
 
 catalogo = {                                                                   #Catalogo de peliculas, divididas por generos 
     "acción": ["Mad Max", "John Wick", "Misión Imposible", "Alerta Roja", ""],
     "terror": ["El Conjuro", "It", "Annabelle"],
-    "comedia": ["Superbad", "Step Brothers", "Ted", "Proyecto X"],
+    "comedia": ["Superbad", "Step Brothers", "Ted", "Proyecto X", "Son como niños", "Son como niños 2"],
     "romance": ["Titanic", "The Notebook", "La La Land"]
 }
 
@@ -16,13 +16,18 @@ ventana.geometry("500x400")
 
 tk.Label(ventana, text="Genero de tu preferencia:").pack()  #Muestra el titulo arriba de donde el usuario va a ingresar el genero
 
-#Crea el espacio donde el usuario va a ingresar el genero
-intro_genero = tk.Entry(ventana, width=40)
-intro_genero.pack()
-
 #crea el boton donde el usuario va a interactuar para ingresar el genero
-boton_enter = tk.Button(ventana, text="Ingresar", background="#b10ee2", foreground="#f2f2f2", command=lambda:generoPel(intro_genero, catalogo, lista_peliculas))
-boton_enter.pack(pady=5)
+frame_botones = tk.Frame(ventana)
+frame_botones.pack(pady=0)
+
+boton_accion = tk.Button(ventana, text="Acción", background="#b10ee2", foreground="#f2f2f2", command=lambda: mostrar_genero("acción",lista_peliculas, catalogo))
+boton_terror = tk.Button(ventana, text="Terror", background="#b10ee2", foreground="#f2f2f2", command=lambda: mostrar_genero("terror",lista_peliculas,catalogo))
+boton_comedia = tk.Button(ventana, text="Comedia", background="#b10ee2", foreground="#f2f2f2", command=lambda: mostrar_genero("comedia",lista_peliculas,catalogo))
+boton_romance = tk.Button(ventana, text="Romance", background="#b10ee2", foreground="#f2f2f2", command=lambda: mostrar_genero("romance", lista_peliculas,catalogo))
+boton_romance.pack(side="left",padx=5)
+boton_accion.pack(side="left",padx=5)
+boton_comedia.pack(side="left",padx=5)
+boton_terror.pack(side="left",padx=5)
 
 #Es donde se muestran las peliculas "SI" el genero introducido existe
 lista_peliculas = tk.Listbox(ventana, width=75, height=18)
--- peliculas-diccionario/modelos/genero.py
+++ peliculas-diccionario/modelos/genero.py
@@ -5,14 +5,23 @@ from tkinter import ttk
 #Si esta disponible, se muestra el catalogo de peliculas de ese genero en especifico
 
 
-def generoPel(intro_genero, catalogo, lista_peliculas):
-    genero = intro_genero.get().lower()
-
-    if genero not in catalogo:
-        messagebox.showwarning("Error", "Debes escribir un género")
-        return
-    
+def mostrar_genero(genero,lista_peliculas,catalogo):
+    # limpiar la lista
     lista_peliculas.delete(0, tk.END)
+    
+    # verificar si existe el género
+    if genero in catalogo:
+        # mostrar cada película
+        for pelicula in catalogo[genero]:
+            lista_peliculas.insert(tk.END, pelicula)
+    else:
+        # si no existe
+        messagebox.showwarning("Error", "Ese género no existe")
 
-    for pelicula in catalogo[genero]:
-        lista_peliculas.insert(tk.END, pelicula)
+    
+
+
+
+
+
+
